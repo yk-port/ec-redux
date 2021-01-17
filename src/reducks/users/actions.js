778e948acbf929596e6f2d82ec3cdf1c52cdac1f
Tatsuya <yokokura0413@gmail.com>
2021-01-18 07:47:34 +0900

    feat: actions作成

---
+++ src/reducks/users/actions.js
@@ -0,0 +1,23 @@
+export const SIGN_IN = 'SIGN_IN';
+export const signInAction = (userState) => {
+  return {
+    type: 'SIGN_IN',
+    payload: {
+      isSignedIn: true,
+      uid: userState.uid,
+      userName: userState.username
+    }
+  }
+};
+
+export const SIGN_OUT = 'SIGN_OUT';
+export const signOutAction = () => {
+  return {
+    type: 'SIGN_OUT',
+    payload: {
+      isSignedIn: false,
+      uid: '',
+      userName: ''
+    }
+  }
+};
