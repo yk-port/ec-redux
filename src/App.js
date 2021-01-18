0687488259c0f357db45d3d42a11422bfdd6f417
Tatsuya <yokokura0413@gmail.com>
2021-01-19 07:42:18 +0900

    feat: ReactとReducの接続

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { signInAction } from './reducks/users/actions';
 
 function App() {
+  const dispatch = useDispatch();
+  const selector = useSelector(state => state);
+
+  console.log(selector.users);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +24,12 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={() => dispatch(signInAction({
+          uid: '0001',
+          username: 'yokokura'
+        }))}>
+          Sign In
+        </button>
       </header>
     </div>
   );
