6d955d57f3a386e9979dece21e5c7e1fc48fc03a
Tatsuya <yokokura0413@gmail.com>
2021-01-20 09:13:10 +0900

    feat: re-ducksパターンを使ったファイル構成の生成

--- src/templates/Home.jsx
+++ src/templates/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
+import { getUserId } from '../reducks/users/selectors';
 
 const Home = () => {
+  const selector = useSelector(state => state);
+  const uid = getUserId(selector);
+
   return (
     <div>
       <h2>Home</h2>
+      <p>{uid}</p>
     </div>
   )
 }
--- src/templates/Login.jsx
+++ src/templates/Login.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { push } from 'connected-react-router';
+import { signInAction } from '../reducks/users/actions';
 
 const Login = () => {
   const dispatch = useDispatch();
   const selector = useSelector(state => state);
 
-  console.log(selector.router);
-
   return (
     <div>
       <h2>ログイン</h2>
-      <button onClick={() => dispatch(push('/'))}>
+      <button onClick={() => {
+        dispatch(signInAction({ uid: '0001', userName: 'yokokura' }));
+        dispatch(push('/'));
+      }}>
         ログインする
       </button>
     </div>
