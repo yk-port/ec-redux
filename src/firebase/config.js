68e79af1d0901216388b0bd402e0395d3c928a81
Tatsuya <yokokura0413@gmail.com>
2021-01-23 04:51:21 +0900

    setting: firebaseの初期化

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyB0SPKyuzDNlAb3jtpSSKAhXm_k3EVTcCI",
+  authDomain: "ec-app-by-redux.firebaseapp.com",
+  projectId: "ec-app-by-redux",
+  storageBucket: "ec-app-by-redux.appspot.com",
+  messagingSenderId: "62938260132",
+  appId: "1:62938260132:web:7a4068f1beb15bddd07c31"
+};
