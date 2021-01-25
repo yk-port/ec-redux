0e6174dd15fd4fb86f50d2b1d0b932da747517ee
Tatsuya <yokokura0413@gmail.com>
2021-01-25 09:00:11 +0900

    feat: サインアウトボタンの設置

--- src/templates/Home.jsx
+++ src/templates/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout } from '../reducks/users/operations';
 import { getUserId, getUserName } from '../reducks/users/selectors';
 
 const Home = () => {
+  const dispatch = useDispatch();
   const selector = useSelector(state => state);
   const uid = getUserId(selector);
   const userName = getUserName(selector);
@@ -12,6 +14,7 @@ const Home = () => {
       <h2>Home</h2>
       <p>{uid}</p>
       <p>{userName}</p>
+      <button onClick={() => dispatch(logout())}>SIGN OUT</button>
     </div>
   )
 }
