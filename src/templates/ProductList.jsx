40319f0c5093cdab45ee603213d7bba640dd70a3
Tatsuya <yokokura0413@gmail.com>
2021-01-30 05:24:48 +0900

    fix: パスの修正

--- src/templates/ProductList.jsx
+++ src/templates/ProductList.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { ProductCard } from '../components/Products';
 import { fetchProducts } from '../reducks/products/operations';
-import { getProducts } from '../reducks/products/selectors ';
+import { getProducts } from '../reducks/products/selectors';
 
 const ProductList = () => {
   const dispatch = useDispatch();
